9b80f790fe48b8fbbf0f9e57038ce748dca90ebd
Vikas Kashyap <126238976+vikaskashyap7@users.noreply.github.com>
2025-04-27 17:45:11 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://expense-tracker-vdh8.onrender.com/api' });
 
 export const fetchExpenses = () => API.get('/expenses');
 export const createExpense = (newExpense) => API.post('/expenses', newExpense);
